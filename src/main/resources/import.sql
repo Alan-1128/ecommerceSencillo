64a26405afe81c33b65d6ccdefd2e6b430e33571
Alan Gonzalez <castillo052899@gmail.com>
2022-02-05 22:37:26 -0600

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO categorias (nombre) VALUES('Celulares');
+-- INSERT INTO categorias (nombre) VALUES('Computadoras');
+-- INSERT INTO categorias (nombre) VALUES('Audifonos');
+
+
+-- INSERT INTO productos (nombre, precio, imagen, categoria_id) VALUES('Motorola One', 4000,  NOW(), 1);
